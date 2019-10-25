4de3cd56c8af78865b92e8a39faa223960a2b45e
hbh112233abc <hbh112233abc@163.com>
2019-10-25 17:06:31 +0800

    port intval处理

--- src/Cas.php
+++ src/Cas.php
@@ -42,7 +42,7 @@ class Cas
             \phpCAS::setDebug('');
             \phpCAS::setVerbose(false); //是否显示错误信息
         }
-        \phpCAS::client($this->conf['cas_version'], $this->conf['host'], $this->conf['port'], $this->conf['context']); //配置客户端
+        \phpCAS::client($this->conf['cas_version'], $this->conf['host'], intval($this->conf['port']), $this->conf['context']); //配置客户端
         \phpCAS::setLang($this->conf['lang']); //支持中文
         \phpCAS::setNoCasServerValidation();
         if (is_file($this->conf['ca_cert_file'])) {
